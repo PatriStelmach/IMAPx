036f06907997149a9ad2744c211f1c63da9ecd50
PatriStelmach <patrzy108@gmail.com>
2025-03-07 14:54:32 +0100

    startup

---
+++ src/main/java/Patri/Stelmach/demo/ImaPxApplication.java
@@ -0,0 +1,13 @@
+package Patri.Stelmach.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaPxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaPxApplication.class, args);
+	}
+
+}
