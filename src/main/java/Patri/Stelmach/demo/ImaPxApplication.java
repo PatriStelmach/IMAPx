e2f34f3ad69f7f68e759900cbe6afb113abec453
PatriStelmach <patrzy108@gmail.com>
2025-03-13 02:24:44 +0100

    ready exe and jar

--- src/main/java/Patri/Stelmach/demo/ImaPxApplication.java
+++ src/main/java/Patri/Stelmach/demo/ImaPxApplication.java
@@ -1,8 +1,12 @@
 package Patri.Stelmach.demo;
 
+import Patri.Stelmach.demo.Frontend.EmailClientApp;
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -11,8 +15,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties()
 public class ImaPxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImaPxApplication.class, args);
+	public static void main(String[] args)
+	{
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(ImaPxApplication.class)
+				.headless(false)
+				.run(args);
+
+
+		Application.launch(EmailClientApp.class, args);
 	}
+
 
 }
